ce52cd7f72ff9e78b466130a63ab1145fa14de1c
Greg Wilson <79663385+gregwdata@users.noreply.github.com>
2023-02-18 04:16:21 +0000

    add year column

--- transform/models/raw/raw_pufs.sql
+++ transform/models/raw/raw_pufs.sql
@@ -3,4 +3,5 @@
 }}
 
 SELECT *
-FROM {{ source( 'eeo1_pufs', 'eeo1_pufs_cleaned' ) }}
+,regexp_extract(FILENAME,'_(\d+)_',1)::integer as YEAR -- get year from filename
+FROM read_csv_auto({{ source( 'eeo1_pufs', 'eeo1_pufs_cleaned' ) }},FILENAME=True,header=True)
